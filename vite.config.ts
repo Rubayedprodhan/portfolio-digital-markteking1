9f90496d39b61e62f9b7fc90f67c036e0573b603
Md Rubayed Prodhan <runabayedprodhan550@gmail.com>
2025-10-17 19:37:08 +0600

    Update project code

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,28 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react-swc";
+// import path from "path";
+// import { componentTagger } from "lovable-tagger";
+
+// // https://vitejs.dev/config/
+// export default defineConfig(({ mode }) => ({
+//   server: {
+//     host: "::",
+//     port: 8080,
+//   },
+//   plugins: [
+//     react(),
+//     mode === "development" && componentTagger()
+//   ].filter(Boolean),
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+//   build: {
+//     outDir: "dist", 
+//   },
+// }));
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
@@ -19,6 +44,34 @@ export default defineConfig(({ mode }) => ({
     },
   },
   build: {
-    outDir: "dist", 
+    outDir: "dist",
+    target: "esnext",          // modern JS → faster build
+    cssCodeSplit: true,        // split CSS → smaller chunks
+    sourcemap: false,          // no source maps → faster
+    minify: "esbuild",         // fastest minifier
+    chunkSizeWarningLimit: 2000,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
+        },
+      },
+    },
+  },
+  optimizeDeps: {
+    include: [
+      "react",
+      "react-dom",
+      "clsx",
+      "date-fns",
+      "react-hook-form",
+      "recharts",
+    ],
   },
 }));
